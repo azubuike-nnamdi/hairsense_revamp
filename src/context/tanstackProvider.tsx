d1069c0f2f499095e3fc2b8b6806afa0ee998ab9
Nnamdi Azubuike <127966002+nnamdi-azubuike@users.noreply.github.com>
2024-06-17 09:54:43 +0100

    chore: config tanstack provider :green_heart:

---
+++ src/context/tanstackProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { useState, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface TanstackProviderProps {
+  children: ReactNode;
+}
+
+const TanstackProvider = ({ children }: TanstackProviderProps) => {
+  const [queryClient] = useState(() => new QueryClient({}));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
