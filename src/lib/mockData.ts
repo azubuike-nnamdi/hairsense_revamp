eb5b0362abc60ee65fe8d65982f918f4011e542b
Nnamdi Azubuike <127966002+nnamdi-azubuike@users.noreply.github.com>
2024-06-18 21:28:02 +0100

    chore: mock footer data :green_heart:

---
+++ src/lib/mockData.ts
@@ -0,0 +1,80 @@
+export const FooterData = [
+  {
+    id: 1,
+    title: "General Information",
+    children: [
+      {
+        id: 1,
+        title: "Terms and Conditions",
+        url: "/terms-and-conditions",
+      },
+      {
+        id: 2,
+        title: "Shipping and payments",
+        url: "/shipping-and-payments",
+      },
+      {
+        id: 3,
+        title: "Delivery Information",
+        url: "/delivery-information",
+      },
+      {
+        id: 4,
+        title: "Privacy",
+        url: "/privacy",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Services",
+    children: [
+      {
+        id: 1,
+        title: "Help and contact",
+        url: "/contact-us",
+      },
+      {
+        id: 2,
+        title: "FAQ",
+        url: "/faq",
+      },
+      {
+        id: 3,
+        title: "Returns",
+        url: "/",
+      },
+      {
+        id: 4,
+        title: "Beauty Guide",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Why Patronize Us",
+    children: [
+      {
+        id: 1,
+        title: "Secure shopping",
+        url: "/secure-shopping",
+      },
+      {
+        id: 2,
+        title: "Shipping and returns",
+        url: "/shipping--and-returns",
+      },
+      {
+        id: 3,
+        title: "Best deals",
+        url: "/best-deals",
+      },
+      {
+        id: 4,
+        title: "Blog",
+        url: "/blog",
+      },
+    ],
+  },
+];
