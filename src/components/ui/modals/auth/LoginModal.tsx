9d293c634324eb44d0399de0ed1de95a34b85415
Nnamdi Azubuike <127966002+nnamdi-azubuike@users.noreply.github.com>
2024-06-19 17:01:49 +0100

    chore: rename auth modal :hammer:

---
+++ src/components/ui/modals/auth/LoginModal.tsx
@@ -0,0 +1,20 @@
+import { X } from "lucide-react";
+import React from "react";
+
+type Props = {
+  onClose: () => void;
+};
+export default function LoginModal({ onClose }: Readonly<Props>) {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="sm:w-5/12 w-full  bg-white p-4 rounded">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-bold">Login</h2>
+          <X size={12} className="cursor-pointer" onClick={onClose} />
+        </div>
+
+        {/* Add  login form here */}
+      </div>
+    </div>
+  );
+}
